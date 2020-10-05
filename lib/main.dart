b826b395ac2942131b33bc678de82bc07e7361b0
somayasamra <72385025+somayasamra@users.noreply.github.com>
2020-10-05 16:19:26 +0400

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Home(),
+  ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+       title: Text('im stupid'),
+       centerTitle: true,
+       backgroundColor: Colors.indigo[600],
+     ),
+      body: Row(
+        children: <Widget>[
+          Expanded(
+              child: Image.asset('Assets.sky2.jpg')
+
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child:Text('1'),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.pinkAccent,
+              child:Text('2'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.amber,
+              child:Text('3'),
+            ),
+          ),
+        ],
+      ),
+    floatingActionButton: FloatingActionButton(
+    onPressed: () {},
+  child: Text('clickdy click'),
+  backgroundColor: Colors.indigo[600],
+  ),
+  );
+  }
+}
+
+
+
+
